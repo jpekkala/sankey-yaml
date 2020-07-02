a424d8cf4170aaf84349375766ef94bebf968460
Jukka Pekkala <jukka@jpekkala.com>
2020-07-02 19:23:01 +0300

    Refactor input parser

--- inputParser.js
+++ inputParser.js
@@ -5,18 +5,16 @@ const d3 = require('d3');
 const DEFAULT_WIDTH = 800
 const DEFAULT_HEIGHT = 600
 
-const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
 exports.parseFromYamlFile = function(fileName) {
     const text = fs.readFileSync(fileName, 'utf8')
     const yaml = jsYaml.load(text)
     const yamlNodes = yaml.nodes
     const nodeMap = new Map(yamlNodes.map(node => [node.name, node]))
     yamlNodes.forEach(addMissingLinkTargets)
-    const orderedNodes = orderNodes()
-    orderedNodes.forEach(colorNode)
+    const orderedNodes = orderNodes(yamlNodes, nodeMap)
+    orderedNodes.forEach(nodeColorer())
 
-    const processedLinks = orderedNodes.flatMap(convertLinks)
+    const processedLinks = orderedNodes.flatMap(linkConverter(nodeMap))
     return {
         unit: yaml.unit,
         width: yaml.width || DEFAULT_WIDTH,
@@ -36,40 +34,10 @@ exports.parseFromYamlFile = function(fileName) {
             }
         })
     }
+}
 
-    function orderNodes() {
-        const orderedNames = []
-        yamlNodes.forEach(recurse)
-        return orderedNames.map(name => nodeMap.get(name))
-
-        function recurse(node) {
-            const { name } = node
-            if (!orderedNames.includes(name)) {
-                orderedNames.push(name)
-            }
-            const children = (node.links || []).map(link => nodeMap.get(link.to))
-            children.forEach(child => child.parent = node)
-            children.forEach(recurse)
-        }
-    }
-
-    function colorNode(node) {
-        if (!node.color) {
-            const {parent} = node
-            if (parent) {
-                colorNode(parent)
-                node.color = parent.color
-            } else {
-                node.color = colorScale(node.name)
-            }
-        }
-
-        if (node.color === 'random') {
-            node.color = colorScale(node.name)
-        }
-    }
-
-    function convertLinks(node) {
+function linkConverter(nodeMap) {
+    return function(node) {
         return (node.links || []).map(link => {
             const targetNode = nodeMap.get(link.to)
             return {
@@ -79,5 +47,41 @@ exports.parseFromYamlFile = function(fileName) {
                 color: targetNode.color || node.color,
             }
         })
+    }
+}
+
+function orderNodes(originalNodes, nodeMap) {
+    const orderedNames = []
+    originalNodes.forEach(recurse)
+    return orderedNames.map(name => nodeMap.get(name))
+
+    function recurse(node) {
+        const { name } = node
+        if (!orderedNames.includes(name)) {
+            orderedNames.push(name)
+        }
+        const children = (node.links || []).map(link => nodeMap.get(link.to))
+        children.forEach(child => child.parent = node)
+        children.forEach(recurse)
+    }
+}
+
+function nodeColorer() {
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    return function colorNode(node) {
+        if (!node.color) {
+            const {parent} = node
+            if (parent) {
+                colorNode(parent)
+                node.color = parent.color
+            } else {
+                node.color = colorScale(node.name)
+            }
+
+            if (node.color === 'random') {
+                node.color = colorScale(node.name)
+            }
+        }
     }
 }
