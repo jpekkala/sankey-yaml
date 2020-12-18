910022c1308c00b0080fe159f60bdd2c8f839546
Jukka Pekkala <jukka@jpekkala.com>
2020-12-19 01:54:17 +0200

    Convert inputParser to classes

--- src/inputParser.js
+++ src/inputParser.js
@@ -5,29 +5,6 @@ const d3 = require('d3');
 const DEFAULT_WIDTH = 800
 const DEFAULT_HEIGHT = 600
 
-class Node {
-    constructor({ name, description, color}) {
-        this.name = name
-        this.description = description
-        this.color = color
-
-        this.incoming = []
-        this.outgoing = []
-    }
-}
-
-class Graph {
-
-    constructor() {
-        this.roots = []
-        this.nodeMap = new Map()
-    }
-
-    addNode(node) {
-        this.nodeMap.set(node.name, node)
-    }
-}
-
 exports.parseFromYamlFile = function(fileName) {
     const text = fs.readFileSync(fileName, 'utf8')
     return parseYaml(text)
@@ -36,82 +13,116 @@ exports.parseFromYamlFile = function(fileName) {
 function parseYaml(text) {
     const yaml = jsYaml.load(text)
     const yamlNodes = yaml.nodes
-    const nodeMap = new Map(yamlNodes.map(node => [node.name, node]))
-    yamlNodes.forEach(addMissingLinkTargets)
-    const orderedNodes = orderNodes(yamlNodes, nodeMap)
-    orderedNodes.forEach(nodeColorer())
 
-    const processedLinks = orderedNodes.flatMap(linkConverter(nodeMap))
+    const graph = new Graph()
+    yamlNodes.forEach(node => graph.addNode(node))
+    graph.autovivifyNodes()
+    graph.reorder()
+    graph.colorNodes()
 
     return {
         title: yaml.title,
         unit: yaml.unit,
         width: yaml.width || DEFAULT_WIDTH,
         height: yaml.height ||DEFAULT_HEIGHT,
-        nodes: orderedNodes,
-        links: processedLinks,
-    }
-
-    function addMissingLinkTargets(node) {
-        const links = node.links || []
-        links.forEach(link => {
-            const targetName = link.to
-            if (!nodeMap.has(targetName)) {
-                nodeMap.set(targetName, {
-                    name: targetName,
-                })
-            }
-        })
+        nodes: graph.nodes,
+        links: graph.links,
     }
 }
 exports.parseYaml = parseYaml
 
-function linkConverter(nodeMap) {
-    return function(node) {
-        return (node.links || []).map(link => {
-            const targetNode = nodeMap.get(link.to)
-            return {
-                source: node.name,
-                target: targetNode.name,
-                value: link.value,
-                color: targetNode.color || node.color,
+class Node {
+    constructor(data) {
+        this.name = data.name
+        this.description = data.description
+        this.color = data.color
+        this.links = data.links || []
+    }
+}
+
+class Graph {
+
+    constructor() {
+        this.nodeMap = new Map()
+    }
+
+    addNode(node) {
+        if (!(node instanceof Node)) {
+            node = new Node(node)
+        }
+        this.nodeMap.set(node.name, node)
+    }
+
+    autovivifyNodes() {
+        const missingTargets = this.nodes
+            .flatMap(node => node.links)
+            .map(link => link.to)
+            .filter(target => !this.nodeMap.has(target))
+
+        for (const target of new Set(missingTargets)) {
+            this.addNode({
+                name: target,
+            })
+        }
+    }
+
+    reorder() {
+        const recurse = (node)  => {
+            const { name } = node
+            if (!map.has(name)) {
+                map.set(name, node)
             }
+            const children = (node.links || []).map(link => this.nodeMap.get(link.to))
+            children.forEach(child => child.parent = node)
+            children.forEach(recurse)
+        }
+
+        const map = new Map()
+        for (const node of this.nodes) {
+            recurse(node)
+        }
+        this.nodeMap = map
+    }
+
+    colorNodes() {
+        const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+        for (const node of this.nodes) {
+            colorNode(node)
+        }
+
+        function colorNode(node) {
+            if (!node.color) {
+                const { parent } = node
+                if (parent) {
+                    colorNode(parent)
+                    node.color = parent.color
+                } else {
+                    node.color = colorScale(node.name)
+                }
+
+                if (node.color === 'random') {
+                    node.color = colorScale(node.name)
+                }
+            }
+        }
+    }
+
+    get nodes() {
+        return Array.from(this.nodeMap.values())
+    }
+
+    get links() {
+        return this.nodes.flatMap(node => {
+            return node.links.map(link => {
+                const targetNode = this.nodeMap.get(link.to)
+                return {
+                    source: node.name,
+                    target: targetNode.name,
+                    value: link.value,
+                    color: targetNode.color || node.color,
+                }
+            })
         })
-    }
-}
-
-function orderNodes(originalNodes, nodeMap) {
-    const orderedNames = []
-    originalNodes.forEach(recurse)
-    return orderedNames.map(name => nodeMap.get(name))
-
-    function recurse(node) {
-        const { name } = node
-        if (!orderedNames.includes(name)) {
-            orderedNames.push(name)
-        }
-        const children = (node.links || []).map(link => nodeMap.get(link.to))
-        children.forEach(child => child.parent = node)
-        children.forEach(recurse)
-    }
-}
-
-function nodeColorer() {
-    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-    return function colorNode(node) {
-        if (!node.color) {
-            const {parent} = node
-            if (parent) {
-                colorNode(parent)
-                node.color = parent.color
-            } else {
-                node.color = colorScale(node.name)
-            }
-
-            if (node.color === 'random') {
-                node.color = colorScale(node.name)
-            }
-        }
     }
 }
