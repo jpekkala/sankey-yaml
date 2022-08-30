a82f6c96b0906e47553c2018d58ac9bc9f94eacb
Jukka Pekkala <jukka@jpekkala.com>
2022-08-30 12:40:05 +0300

    Register plugins

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-const fs = require('fs')
-const process = require('process')
-
 const {
     generateFromFile,
     generateFromFolder,
 } = require('./src/svgDrawer')
 
-const SHEET_FOLDER = './sheets'
+const {
+    registerPlugin,
+} = require('./src/plugins')
 
-const { Command } = require('commander')
-const program = new Command()
-program
-    .name('budget')
-    .description('Generates Sankey charts from YAML files')
-    .argument('[in]', 'The file or folder where to read YAML from', SHEET_FOLDER)
-    .parse()
+exports.generateFromFile = generateFromFile
+exports.generateFromFolder = generateFromFolder
+exports.registerPlugin = registerPlugin
+
+function main() {
+    const SHEET_FOLDER = './sheets'
+
+    const fs = require('fs')
+    const process = require('process')
+    const { Command } = require('commander')
+    const program = new Command()
+    program
+        .name('budget')
+        .description('Generates Sankey charts from YAML files')
+        .argument('[in]', 'The file or folder where to read YAML from', SHEET_FOLDER)
+        .parse()
 
 
-const [inputPath] = program.processedArgs
+    const [inputPath] = program.processedArgs
 
-if (!fs.existsSync(inputPath)) {
-    console.error(`The input path "${inputPath}" is not file or folder`)
-    process.exit(1)
+    if (!fs.existsSync(inputPath)) {
+        console.error(`The input path "${inputPath}" is not file or folder`)
+        process.exit(1)
+    }
+
+    const stat = fs.lstatSync(inputPath)
+    if (stat.isDirectory()) {
+        generateFromFolder(inputPath)
+    } else if (stat.isFile()) {
+        generateFromFile(inputPath)
+    } else {
+        throw Error('Invalid file type')
+    }
 }
 
-const stat = fs.lstatSync(inputPath)
-if (stat.isDirectory()) {
-    generateFromFolder(inputPath)
-} else if (stat.isFile()) {
-    generateFromFile(inputPath)
-} else {
-    throw Error('Invalid file type')
+if (require.main === module) {
+    main();
 }
--- src/plugins.js
+++ src/plugins.js
@@ -1,7 +1,25 @@
+const {
+    isFunction,
+} = require('lodash')
+
+const plugins = {}
+
+exports.registerPlugin = function(name, pluginFn) {
+    if (!isFunction(pluginFn)) {
+        throw Error('pluginFn should be a function')
+    }
+    plugins[name] = pluginFn
+}
+
 exports.callLinkPlugin = function(link, valueProp) {
     const { plugin: pluginName } = valueProp
     if (!pluginName) {
         throw Error('Prop "plugin" missing in value declaration')
+    }
+
+    const pluginFn = plugins[pluginName]
+    if (pluginFn) {
+        return pluginFn(link, valueProp)
     }
 
     throw Error(`Unknown plugin ${ pluginName }`)
